5b63bcdfd28b77bcc324f2e297e8638a5e7d2be7
Charlotte Astle <charlotteastle@Charlottes-MBP.att.net>
2017-07-17 09:16:19 -0700

    creates create cloze function

--- clozecard.js
+++ clozecard.js
@@ -7,6 +7,22 @@ var BasicCard = require('./basiccard.js');
 var ClozeCard = function(text, cloze){
 	this.text = text;
 	this.cloze = cloze;
+	this.clozeDeleted =;
+	this.create = function(){
+		var cardData = {
+			type : 'cloze',
+			text : this.text,
+			cloze : this.cloze,
+			clozeDeleted: this.clozeDeleted;
+		};
+
+		//Append card data to text file
+		fs.appendFile('questions.txt', JSON.stringify(cardData) + ';','utf8', function(error){
+			if(error){
+				console.log(error);
+			}
+		})
+	};
 };
 
 //Exports the constructor
